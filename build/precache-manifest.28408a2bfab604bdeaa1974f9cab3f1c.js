1960b00f1af6f0ce04b1db84b93643c01b34d65f
abhidn <62213800+abhidn@users.noreply.github.com>
2022-12-12 17:54:19 +0530

    Add files via upload

---
+++ build/precache-manifest.28408a2bfab604bdeaa1974f9cab3f1c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bfbe6eb73da7552cfb356a2c6d66d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6600f5fa31c5198990fd",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "3d11bb58902ca417dbc5",
+    "url": "/static/css/main.19ed70a6.chunk.css"
+  },
+  {
+    "revision": "6600f5fa31c5198990fd",
+    "url": "/static/js/2.d1cd0c4f.chunk.js"
+  },
+  {
+    "revision": "0fee192dfb15f6872103ebb6678e1c43",
+    "url": "/static/js/2.d1cd0c4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d11bb58902ca417dbc5",
+    "url": "/static/js/main.56a0d237.chunk.js"
+  },
+  {
+    "revision": "4f6e34b3be3410815ed1",
+    "url": "/static/js/runtime-main.07b96789.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "0e651ff6c9928dad413931ce4ed2057b",
+    "url": "/static/media/green-chameleon-s9CC2SKySJM-unsplash.0e651ff6.jpg"
+  }
+]);
